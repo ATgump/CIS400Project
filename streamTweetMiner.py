c50250ddf1e63d61a5e46ed90ad2e0ab7a1a9777
ATGump <averygmp@gmail.com>
2021-04-17 18:29:07 -0400

    Added language filter
    Only collects english tweets now and has some queries for restraunts added/formatted to input to filter

--- streamTweetMiner.py
+++ streamTweetMiner.py
@@ -13,14 +13,24 @@ twitter_stream = twitter.TwitterStream(auth=twitter_api.auth)
 # Query terms
 q = "covid,'COVID-19'"
 
-print('Filtering the public timeline for track={0}'.format(q), file=sys.stderr)
+#removed : claro
+q2 = "'Five Guys',Marios,Sylvias,Defontes,Patsys,Barbetta,Keens,Katzs,\
+Bamontes,Frenchette,'The Modern',Cookshop,'Casa Lever','Mercer Kitchen',\
+'Charlie Bird','Le Bernardin',Tong,'Runaway Roof','La Grenouille',\
+'P.J. Clarkes',Soothr,Raouls,Forsynthia,'Wo Hop',Adda,'Crown Shy',\
+Mokyo,'Via Carota',Atoboy,'Usha Foods','Peter Luger','The Rainbow Room',\
+Totonnos,Delmonicos,Raos,'Nathans Famous','Johns Pizzeria'"
+
+q3 = "McDonalds,Wendys,'Burger King',Subway,'Pizza Hut'"
+
+print('Filtering the public timeline for track={0}'.format(q3), file=sys.stderr)
 sys.stderr.flush()
 
 # See https://developer.twitter.com/en/docs/tutorials/consuming-streaming-data
 # stream = twitter_stream.statuses.filter(track=q)
 #stream = make_twitter_request(twitter_stream.statuses.filter, track=q)
 #stream = make_twitter_request(twitter_stream.statuses.filter, track=q, delimited='length', stallings = True)
-stream = make_twitter_request(twitter_stream.statuses.filter, track=q, stallings = True)
+stream = make_twitter_request(twitter_stream.statuses.filter, track=q3, stallings = True)
 
 # For illustrative purposes, when all else fails, search for Justin Bieber
 # and something is sure to turn up (at least, on Twitter)
@@ -30,7 +40,7 @@ tweet_list = []
 
 
 mConnection = 'mongodb+srv://CISProjectUser:U1WsTu2X6fix49PA@cluster0.ttjkp.mongodb.net/test?authSource=admin&replicaSet=atlas-vvszkk-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
-
+# box for nyc : -74,40,-73,41
 i = 0
 
 for tweet in stream:
@@ -44,7 +54,7 @@ for tweet in stream:
             print("broke")
             break
         elif not(type(tweet) is int):
-            if not(tweet.get('retweeted_status',False)):
+            if not(tweet.get('retweeted_status',False)) and tweet['lang'] == 'en':
                 print("appended: " + tweet['text'][:10])
                 db_add = {}
                 db_add['_id'] = tweet['id']
@@ -54,5 +64,5 @@ for tweet in stream:
                 tweet_list.append(db_add)   
         print (tweet['text']) 
     except:
-        print("passed")
+        #print("passed")
         pass
