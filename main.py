e2d40df111fa5eb84e2c5ccecf18d8d10560fbe8
ATGump <averygmp@gmail.com>
2021-04-19 21:25:35 -0400

    Calculate TFIDF using sk learn
    Used sk learn to extract TFIDF feature vectors from corpus

--- main.py
+++ main.py
@@ -1,2 +1,60 @@
+import myTwitterCookbook
+import pandas as pd
+import numpy as np
+import pymongo
+import json
+from sklearn import feature_extraction as sk
+from nltk.corpus import twitter_samples as tweets
+import ourCorpus
 #mongo connection
 client = pymongo.MongoClient('mongodb+srv://CISProjectUser:U1WsTu2X6fix49PA@cluster0.ttjkp.mongodb.net/test?authSource=admin&replicaSet=atlas-vvszkk-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+db = client['test_Search_Mining_DB']
+
+#get the list[{full_text : tweet}]
+cursor = db['T'].find({},{'extended_tweet':{'full_text':1},'_id':0})
+
+
+neg_tweets = tweets.strings("negative_tweets.json")
+pos_tweets = tweets.strings("positive_tweets.json")
+raw_docs = neg_tweets + pos_tweets
+
+raw_docs = [ourCorpus.normalize_tweet(docs) for docs in raw_docs]
+print("\n\n\n")
+l = [et['extended_tweet'] for et in list(cursor)]
+l2 = [ourCorpus.normalize_tweet(et['full_text']) for et in l]
+
+## fit to matrix
+cv = sk.text.CountVectorizer(min_df = 0.01,max_df=0.85,stop_words=sk._stop_words.ENGLISH_STOP_WORDS)
+word_count_vector = cv.fit_transform(raw_docs)
+tfidf_transformer = sk.text.TfidfTransformer(smooth_idf = True, use_idf = True)
+test = tfidf_transformer.fit_transform(word_count_vector)
+tframe = pd.DataFrame(test.toarray(),columns = cv.get_feature_names())
+print(tframe)
+
+
+print("THIS IS L2\n\n" + str(l2) + "\n\n")
+
+# print(pd.DataFrame(mat))
+# print(mat[0][0].toarray())
+#col = myTwitterCookbook.load_from_mongo('test_Search_Mining_DB', 'T', q, host=client)
+# for row in mat:
+#     for entry in row:
+#         print(entry[1])
+
+
+
+
+
+
+#vectorizor = sk.text.TfidfVectorizer()
+#vectorizor = sk.text.TfidfVectorizer(max_features=100, stop_words='english')
+#mat2 = vectorizor.fit_transform(raw_documents = raw_docs)
+#print(vectorizor.get_feature_names())
+#neg_mat = vectorizor.transform(raw_documents = neg_tweets)
+#pos_mat = vectorizor.transform(raw_documents = pos_tweets)
+#tokenizer = vectorizor.build_tokenizer()
+#train_d = mat2
+#train_d = [dict({'text':neg,'class':0}) for neg in neg_mat] + [dict({'text':pos,'class':1}) for pos in pos_mat]
+#frame = pd.DataFrame(columns = vectorizor.get_feature_names())
+#frame2 = pd.DataFrame(train_d)
+#mat = vectorizor.fit_transform(raw_documents = l2)
