fb5736426977c13acb04dd23a391a73d3e7ab185
ATGump <averygmp@gmail.com>
2021-04-22 06:20:17 -0400

    Fixed Lemmatizer
    Implemented parallel processing to do lemmatization of data as well as spelling correction. 10000 samples took ~2hrs with prior method now takes ~2mins.

--- ourCorpus.py
+++ ourCorpus.py
@@ -15,37 +15,47 @@ bigram_path = pkg_resources.resource_filename("symspellpy", "frequency_bigramdic
 sym_spell.load_dictionary(dictionary_path, term_index=0, count_index=1)
 sym_spell.load_bigram_dictionary(bigram_path, term_index=0, count_index=2)
 
-    # tweet_list = [ele for ele in tweet.split() if ele != 'user']
-    # clean_tokens = [t for t in tweet_list if re.match(r'[^\W\d]*$', t)]
-    # clean_s = ' '.join(clean_tokens)
-    # clean_mess = [word for word in clean_s.split() if word.lower() not in stopwords.words('english')]
-
-#@[A-Za-z0–9]+
+from joblib import Parallel, delayed
 import spacy
-test_sentence = 'How am   I just finding this awesome template by @54Mr_Meyer?! Snag your own copy: https://t.co/DZOQovcLFl @SlidesManiaSM #Free resources for #GoogleSlides &amp; #PowerPoint @test'
+test_sentence = 'How am   I just finkding thi\'s awegsome templatnbe by @54Mr_Meyer?! Snfag your own copy: https://t.co/DZOQovcLFl @SlidesManiaSM #Free resources for #GoogleSlides &amp; #PowerPoint @test'
 def normalize_tweet(twt):
 	twt = twt.lower()
 	emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',twt) # get emoticons (to add to end important for sentiment removed during further prepro)
+	twt = re.sub(r'(@[^\s]+(\s|$))|(#[^\s]+(\s|$))|(https?://[^\s]+(\s|$))|(&[^\s]+(\s|$))|[^\w\s\']|\d','',twt)# remove @ mentions, #, urls, special characters besides ' , and numbers
+	twt = re.sub(r'\s+',' ',twt)
+	return (twt,emoticons)#twt + ' '.join(emoticons)
+
+def chunker(tweet_list,length,chunksize):
+	return(tweet_list[pos:pos + chunksize] for pos in range(0,length,chunksize)) #return a generator for the chunks
+
+def lemmatize_pipe(doc):
+	lemma_list = [str(tok.lemma_).lower() for tok in doc if tok.is_alpha] #and tok.text.lower() not in stopwords
+	# test spell correction here
+	s = ' '.join(lemma_list)
+	return sym_spell.lookup_compound(s,max_edit_distance=2)[0]._term.split()	
+	#sym_spell.lookup_compound(s, max_edit_distance = 2)[0]._term
+	#lemma_list
+
+def flatten(L):
+    return [item for S in L for item in S]
+
+def chunk_processor(texts):
+	preproc_pipe = []
 	nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
-	twt = re.sub(r'(@[^\s]+(\s|$))|(#[^\s]+(\s|$))|(https?://[^\s]+(\s|$))','',twt)# remove @ mentions, #, and urls 
-	twtblob = twt.split()
-	rm_tokens = [t for t in twtblob if re.match(r'[^\W\d]*$', t)] # removes any word that has non characters in it includeing ones that dont have a space ... maybe add space in between non chars to split words so copy: -> copy : and doesnt get removed
-	sentence = ' '.join(rm_tokens)
-	doc = nlp(re.sub(r'\s+',' ',sentence)) #remove extra spaces from sentence then pass it to spacy to lemmatize
-	return ' '.join([token.lemma_ for token in doc])+' ' + ' '.join(emoticons)
+	for doc in nlp.pipe(texts, batch_size=20):
+		preproc_pipe.append(lemmatize_pipe(doc))
+	return preproc_pipe
 
-start_time = datetime.datetime.now()
+def batch_lemmatizer(texts,chunksize=100):
+	executor = Parallel(n_jobs=4, backend='multiprocessing', prefer="processes")
+	do = delayed(chunk_processor)
+	tasks = (do(chunk) for chunk in chunker(texts, len(texts), chunksize=chunksize))
+	result = executor(tasks)
+	return flatten(result)
+
 print(normalize_tweet(test_sentence))
-end_time = datetime.datetime.now()
-print(end_time-start_time)
 
-
-def batch_lemmatizer(tweet_collection):
-# remove_rt = lambda x: re.sub(‘RT @\w+: ‘,” “,x)
-# rt = lambda x: re.sub(“(@[A-Za-z0–9]+)|([⁰-9A-Za-z \t])|(\w+:\/\/\S+)”,” “,x)
-
-
-
+print(sym_spell.lookup_compound(normalize_tweet(test_sentence)[0], max_edit_distance = 2)[0]._term)
 ## split regexs
 
 	# twt = re.sub(r'\s+',' ',twt) #remove extra white space
