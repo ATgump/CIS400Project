2d354b3a25c4ab4415555c1d5df9d57d89b60827
ATgump <averygmp@gmail.com>
2021-05-08 17:19:46 -0400

    Working on Classifiers
    Optimizing MLP And has the other classifier tests

--- main.py
+++ main.py
@@ -5,94 +5,144 @@ import pymongo
 import json
 import sklearn
 import sklearn.naive_bayes
+from sklearn import svm
+from sklearn.neural_network import MLPClassifier
 from sklearn import feature_extraction as sk
+from sklearn.model_selection import GridSearchCV
 from nltk.corpus import twitter_samples as tweets
 import ourCorpus
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
+import random
+import sys
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import LinearSVC
 #mongo connection
 if __name__ == "__main__":
+    pd.set_option('display.max_rows', 500)
+    n = random.randint(0, 2**32 - 20)
+    #q4 = ["McDonalds","Wendys",'Burger King','Pizza Hut',"Whataburger","In-N-OutBurger","White Castle","Starbucks","Auntie Anne\'s","Popeyes","Chick-fil-A",'Taco Bell',"Arby\'s","Dairy Queen"]
+    #print(ourCorpus.batch_lemmatizer(q4))
+
     client = pymongo.MongoClient('mongodb+srv://CISProjectUser:U1WsTu2X6fix49PA@cluster0.ttjkp.mongodb.net/test?authSource=admin&replicaSet=atlas-vvszkk-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
-    db = client['test_Search_Mining_DB']
-
-#get the list[{full_text : tweet}]
-    cursor = db['T'].find({},{'extended_tweet':{'full_text':1},'_id':0})
+    db = client['tweet_DB']
 
 
-    neg_tweets = tweets.strings("negative_tweets.json")
-    pos_tweets = tweets.strings("positive_tweets.json")
-    # neg_tweets = [ourCorpus.normalize_tweet(docs) for docs in neg_tweets]
-    # neg_tweets = [docs for docs in neg_tweets if not docs[0].isspace()]
-    # pos_tweets = [ourCorpus.normalize_tweet(docs) for docs in pos_tweets]
-    # pos_tweets = [docs for docs in pos_tweets if not docs[0].isspace()]
-    # raw_docs = neg_tweets + pos_tweets
-    # pos_frame = pd.DataFrame(pos_tweets,columns = ['pos_tweet','p_emotes'])
-    # neg_frame = pd.DataFrame(neg_tweets,columns = ['neg_tweet','n_emotes'])
-    # nf = pd.concat([pos_frame,neg_frame],axis = 1)
-    nf = pd.DataFrame()
-    # print(nf)
+    df = pd.DataFrame(list(db['processed_Training_Data_Three'].find({},{'_id':0,'lema_text':1,'label':1})))
+
+
+    vectorizor = sk.text.TfidfVectorizer(min_df=.000027) #stop_words=sk._stop_words.ENGLISH_STOP_WORDS,min_df=.00003,max_df=.94
+    v = vectorizor.fit_transform(df['lema_text'].to_numpy())
+
+
+    #print("Random state = " + str(n))
+    X_train,X_test,y_train,y_test = sklearn.model_selection.train_test_split(v,df['label'],test_size=.2,random_state=n) #tframe.drop('CLASS_ATR',axis = 1),tframe['CLASS_ATR']
     
-    #tf5 = pd.DataFrame(raw_docs)
-    #print(tf5)
-    #raw_docs = ourCorpus.batch_lemmatizer(raw_docs,100)
-    #raw_docs = [twt for (twt,emote) in raw_docs]
-    # nf['pos_lema'] = (pd.Series(ourCorpus.batch_lemmatizer(nf['pos_tweet'],100)) + nf['p_emotes']).apply(lambda x: ' '.join(x))
-    # nf['neg_lema'] = (pd.Series(ourCorpus.batch_lemmatizer(nf['neg_tweet'],100)) + nf['n_emotes']).apply(lambda x: ' '.join(x))
-    nf['pos_lema'] = (pd.Series(ourCorpus.batch_lemmatizer(pos_tweets,100)))
-    nf['neg_lema'] = (pd.Series(ourCorpus.batch_lemmatizer(neg_tweets,100)))
+    
+    # MNB_classifier=sklearn.naive_bayes.MultinomialNB()
+    # MNB_classifier.fit(X_train,y_train)
 
-    # tf5['lema'] = ourCorpus.batch_lemmatizer(tf5[0],100)
-    # tf5['connect'] = (tf5['lema'] + tf5[1]).apply(lambda x: ' '.join(x))
-    #print(nf.drop(columns =['pos_tweet','neg_tweet','p_emotes','n_emotes']))
-    print(nf)
+    mlp = MLPClassifier(max_iter=500) #max_iter=250
+    # mlp.fit(X_train,y_train)
 
-    #print(tf5)
-    #raw_docs = [' '.join(doc) for doc in raw_docs]
-    #print(raw_docs)
-    #raw_docs = [ourCorpus.normalize_tweet(docs) for docs in raw_docs]
-    #raw_docs = [docs for docs in raw_docs if not docs.isspace()]
+    # SV_classifier = svm.SVC()
+    # SV_classifier.fit(X_train,y_train)
 
-    train_d = [dict({'text':neg,'class':0}) for neg in nf['neg_lema']] + [dict({'text':pos,'class':1}) for pos in nf['pos_lema']]
-    frame = pd.DataFrame(train_d)
-    print(frame)
+    # DT_classifier = DecisionTreeClassifier()
+    # DT_classifier.fit(X_train,y_train)
 
-    testframe = (frame.drop('text',axis =1)).to_numpy()
-    #print(testframe)
+    # GNB_classifier = GaussianNB()
+    # GNB_classifier.fit(X_train.toarray(),y_train)
 
-    #l = [et['extended_tweet'] for et in list(cursor)]
-    #l2 = [ourCorpus.normalize_tweet(et['full_text']) for et in l]
+    # RF_classifier = RandomForestClassifier()
+    # RF_classifier.fit(X_train,y_train)
 
-    ## fit to matrix
-    #min_df = 0.01,max_df=0.85
-    #min_df = 0.0002,max_df=.99
-    #print(raw_docs)
-    cv = sk.text.CountVectorizer(stop_words=sk._stop_words.ENGLISH_STOP_WORDS,token_pattern=r'[^\s]+') #token patern matches emoticons now too
-    word_count_vector = cv.fit_transform(frame['text'].to_numpy())
-    print(cv.get_feature_names())
-    tfidf_transformer = sk.text.TfidfTransformer(smooth_idf = True, use_idf = True)
-    test = tfidf_transformer.fit_transform(word_count_vector)
-    tframe = pd.DataFrame(test.toarray(),columns = cv.get_feature_names())
-    tframe['CLASS_ATR'] = testframe
-    print(tframe)
-    X = tframe.drop('CLASS_ATR',axis = 1)
-    y = tframe['CLASS_ATR']
+    # LSV_classifier = LinearSVC()
+    # LSV_classifier.fit(X_train,y_train)
+
+#     print("Multinomial Naive Bayes Classifier Report: \t\t")
+#     pred_MNB = MNB_classifier.predict(X_test)
+#     print(sklearn.metrics.classification_report(y_test,pred_MNB))
+
+    # print("Multi-Layer Perceptron Classifier Report: \t\t")
+    # pred_MLP = mlp.predict(X_test)
+    # print(sklearn.metrics.classification_report(y_test,pred_MLP))
+
+    # print("Support Vector Classifier Report: \t\t")
+    # pred_SV = SV_classifier.predict(X_test)
+    # print(sklearn.metrics.classification_report(y_test,pred_SV))
+# ###
+#     print("Decision Tree Classifier Report: \t\t")
+#     pred_DT = DT_classifier.predict(X_test)
+#     print(sklearn.metrics.classification_report(y_test,pred_DT))
+    
+    # print("Gausian Naive Bayes Classifier Report: \t\t")
+    # pred_GNB = GNB_classifier.predict(X_test.toarray())
+    # print(sklearn.metrics.classification_report(y_test,pred_GNB))
+
+    # print("Random Forest Classifier Report: \t\t")
+    # pred_RF = RF_classifier.predict(X_test)
+    # print(sklearn.metrics.classification_report(y_test,pred_RF))
+
+    # print("Linear Support Vector Classifier Report: \t\t")
+    # pred_LSV = LSV_classifier.predict(X_test)
+    # print(sklearn.metrics.classification_report(y_test,pred_LSV))
 
 
-    X_train,X_test,y_train,y_test = sklearn.model_selection.train_test_split(X,y,test_size=.2,random_state=42)
-    NB_classifier=sklearn.naive_bayes.MultinomialNB()
-    NB_classifier.fit(X_train,y_train)
-    pred_NB = NB_classifier.predict(X_test)
-    print(sklearn.metrics.classification_report(y_test,pred_NB))
+    parameter_space = {
+    'hidden_layer_sizes': 
+    [
+    #(50,50,50,50,50,50,50,50,50,50,50,50,50,50,50), 
+    #(500,500,500),  
+    #(10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10,10),
+    #(64,64,64,64,64,64,64,64), 
+    #(128,128,128,128,128,128,128,128),
+    #(10,10,100,50,20,10),
+    #(80,80,80,80,80,80,80,80),
+    #(32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32,32),
+    #(10,10,10),
+    #(256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256),
+    #(256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,256,256,256,256),
+    (256,256,256,256,256,256,256,256,256),
+    #(256,256,256,256,256,256,256,256,256,256,256,256,128,56,10,6),
+    #(128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128,128),
+    #(500,500)
+    #(200,10,10,10,10,10,10,10,10,200),
+    #(10,200,200,10)                                                                                                            
+    ],
+    'activation':['relu'], #'tanh','identity', 'logistic'
+    'solver':['adam'], #'sgd','lbfgs'
+    'alpha':[.0001], #.0001,.0000000001,.0002,.0007,.0000000000000000000000000000000000000000000000000000000000000000000000001,.05
+    'learning_rate':['adaptive','constant','invscaling'] #'constant','invscaling'
+}
 
-############################################################################################################################## uncomment above
+    clf = GridSearchCV(mlp, parameter_space, n_jobs=-1, cv=3,verbose = 100)
+    clf.fit(X_train, y_train)
 
-#vectorizor = sk.text.TfidfVectorizer()
-#vectorizor = sk.text.TfidfVectorizer(max_features=100, stop_words='english',token_pattern=r'[^\s]+')
-#mat2 = vectorizor.fit_transform(raw_documents = raw_docs)
-#print(vectorizor.get_feature_names())
-#neg_mat = vectorizor.transform(raw_documents = neg_tweets)
-#pos_mat = vectorizor.transform(raw_documents = pos_tweets)
-#tokenizer = vectorizor.build_tokenizer()
-#train_d = mat2
-#train_d = [dict({'text':neg,'class':0}) for neg in neg_mat] + [dict({'text':pos,'class':1}) for pos in pos_mat]
-#frame = pd.DataFrame(columns = vectorizor.get_feature_names())
-#frame2 = pd.DataFrame(train_d)
-#mat = vectorizor.fit_transform(raw_documents = l2)
+
+    # Best paramete set
+    print('Best parameters found:\n', clf.best_params_)
+
+    # All results
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        print("%0.3f (+/-%0.03f) for %r" % (mean, std * 2, params))
+
+
+
+    
+    #print(len(vectorizor.get_feature_names()))
+
+    # tframe = pd.DataFrame(v.toarray(),columns = vectorizor.get_feature_names())
+    # tframe = tframe.T.sum(axis=1)
+    # Cloud = WordCloud(background_color="white", max_words=100,stopwords=STOPWORDS).generate_from_frequencies(tframe)
+    # plt.figure(figsize = (8, 8), facecolor = None)
+    # plt.imshow(Cloud)
+    # plt.axis("off")
+    # plt.tight_layout(pad = 0)
+
+    # plt.show()
+
