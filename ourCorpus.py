587a56ea4d1a549088c09d3a25c0016fda0b573c
ATgump <averygmp@gmail.com>
2021-05-08 17:19:16 -0400

    Fixed URL and html tag removal
    In addition to the title I also removed empty tweets from our training data

--- ourCorpus.py
+++ ourCorpus.py
@@ -7,6 +7,9 @@ import textblob
 import re
 import datetime
 from sklearn import feature_extraction as sk
+from bs4 import BeautifulSoup
+import pymongo
+import pandas as pd
 sym_spell = SymSpell(max_dictionary_edit_distance=2, prefix_length=7)
 dictionary_path = pkg_resources.resource_filename("symspellpy", "frequency_dictionary_en_82_765.txt")
 bigram_path = pkg_resources.resource_filename("symspellpy", "frequency_bigramdictionary_en_243_342.txt")
@@ -19,65 +22,65 @@ from joblib import Parallel, delayed
 import spacy
 from emoji import demojize
 def load_dict_smileys():
-    
-    return {
-        ":‑)":"smiley",
-        ":-]":"smiley",
-        ":-3":"smiley",
-        ":->":"smiley",
-        "8-)":"smiley",
-        ":-}":"smiley",
-        ":)":"smiley",
-        ":]":"smiley",
-        ":3":"smiley",
-        ":>":"smiley",
-        "8)":"smiley",
-        ":}":"smiley",
-        ":o)":"smiley",
-        ":c)":"smiley",
-        ":^)":"smiley",
-        "=]":"smiley",
-        "=)":"smiley",
-        ":-))":"smiley",
-        ":‑D":"smiley",
-        "8‑D":"smiley",
-        "x‑D":"smiley",
-        "X‑D":"smiley",
-        ":D":"smiley",
-        "8D":"smiley",
-        "xD":"smiley",
-        "XD":"smiley",
-        ":‑(":"sad",
-        ":‑c":"sad",
-        ":‑<":"sad",
-        ":‑[":"sad",
-        ":(":"sad",
-        ":c":"sad",
-        ":<":"sad",
-        ":[":"sad",
-        ":-||":"sad",
-        ">:[":"sad",
-        ":{":"sad",
-        ":@":"sad",
-        ">:(":"sad",
-        ":'‑(":"sad",
-        ":'(":"sad",
-        ":‑P":"playful",
-        "X‑P":"playful",
-        "x‑p":"playful",
-        ":‑p":"playful",
-        ":‑Þ":"playful",
-        ":‑þ":"playful",
-        ":‑b":"playful",
-        ":P":"playful",
-        "XP":"playful",
-        "xp":"playful",
-        ":p":"playful",
-        ":Þ":"playful",
-        ":þ":"playful",
-        ":b":"playful",
-        "<3":"love"
-        }
+	
+	return {
+		":‑)":"smiley",
+		":-]":"smiley",
+		":-3":"smiley",
+		":->":"smiley",
+		"8-)":"smiley",
+		":-}":"smiley",
+		":)":"smiley",
+		":]":"smiley",
+		":3":"smiley",
+		":>":"smiley",
+		"8)":"smiley",
+		":}":"smiley",
+		":o)":"smiley",
+		":c)":"smiley",
+		":^)":"smiley",
+		"=]":"smiley",
+		"=)":"smiley",
+		":-))":"smiley",
+		":‑D":"smiley",
+		"8‑D":"smiley",
+		"x‑D":"smiley",
+		"X‑D":"smiley",
+		":D":"smiley",
+		"8D":"smiley",
+		"xD":"smiley",
+		"XD":"smiley",
+		":‑(":"sad",
+		":‑c":"sad",
+		":‑<":"sad",
+		":‑[":"sad",
+		":(":"sad",
+		":c":"sad",
+		":<":"sad",
+		":[":"sad",
+		":-||":"sad",
+		">:[":"sad",
+		":{":"sad",
+		":@":"sad",
+		">:(":"sad",
+		":'‑(":"sad",
+		":'(":"sad",
+		":‑P":"playful",
+		"X‑P":"playful",
+		"x‑p":"playful",
+		":‑p":"playful",
+		":‑Þ":"playful",
+		":‑þ":"playful",
+		":‑b":"playful",
+		":P":"playful",
+		"XP":"playful",
+		"xp":"playful",
+		":p":"playful",
+		":Þ":"playful",
+		":þ":"playful",
+		":b":"playful",
+		"<3":"love"
+		}
 
 
 def emoji_Replacer(twt):
@@ -90,26 +93,17 @@ def emoji_Replacer(twt):
 	#print(tweet)
 	return(tweet)
 
-test_sentence = 'How am   I just finkding thi\'s awegsome templatnbe by @54Mr_Meyer?! Snfag your own copy: https://t.co/DZOQovcLFl @SlidesManiaSM #Free resources for #GoogleSlides &amp; #PowerPoint @test'
-# def normalize_tweet(twt):
-# 	twt = twt.lower()
-# 	twt = emoji_Replacer(twt)
-# 	emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',twt) # get emoticons (to add to end important for sentiment removed during further prepro)
-# 	twt = re.sub(r'(@[^\s]+(\s|$))|(https?://[^\s]+(\s|$))|(&[^\s]+(\s|$))|[^\w\s\']|\d','',twt)# |(#[^\s]+(\s|$)) remove @ mentions, #, urls, special characters besides ' , and numbers
-# 	twt = re.sub(r'\s+',' ',twt)
-# 	return (twt)#,emoticons)#twt + ' '.join(emoticons)
+test_sentence = 'How am   wendy burger king I just finkding thi\'s awegsome templatnbe by @54Mr_Meyer?! Snfag your own copy: https://t.co/DZOQovcLFl @SlidesManiaSM #Free resources for #GoogleSlides &amp; #PowerPoint @test'
 
 def chunker(tweet_list,length,chunksize):
 	return(tweet_list[pos:pos + chunksize] for pos in range(0,length,chunksize)) #return a generator for the chunks
-
+q4 =['mcdonald', 'wendy', 'burger', 'starbuck', 'king', 'pizza', 'hut', 'inonu', 'white', 'castle', 'auntie', 'news', 'popeye', 'chick', 'fila', 'taco', 'bell', 'abyss', 'dairy' ,'queen']
+words = set(q4) | set(stopwords.words('english')) | set(['face'])
 def lemmatize_pipe(doc):
-	lemma_list = [str(tok.lemma_).lower() for tok in doc if tok.is_alpha] #and tok.text.lower() not in stopwords
-	# test spell correction here
+	lemma_list = [str(tok.lemma_).lower() for tok in doc if tok.is_alpha and str(tok.lemma_).lower() not in words] #and tok.text.lower() not in stopwords tok.text.lower()
 	s = ' '.join(lemma_list)
-	#print(s)
 	return s	
-	#sym_spell.lookup_compound(s, max_edit_distance = 2)[0]._term
-	#lemma_list
+
 
 def flatten(L):
 	#print(L)
@@ -120,28 +114,63 @@ def chunk_processor(texts):
 	process = []
 	for twt in texts:
 		#print(twt)
+		twt = re.sub(r"http\S+", "", twt)
+		twt = re.sub(r'https?:\/\/[^\s]+(\s|$)','',twt)
+		twt = re.sub(r'^https?:\/\/.*[\r\n]*', '', twt, flags=re.MULTILINE)
+		twt = BeautifulSoup(twt,features="html.parser").get_text()
 		twt = emoji_Replacer(twt)
-        #emoticons = re.findall('(?::|;|=)(?:-)?(?:\)|\(|D|P)',twt) # get emoticons (to add to end important for sentiment removed during further prepro)
-		twt = re.sub(r'(@[^\s]+(\s|$))|(#[^\s]+(\s|$))|(https?://[^\s]+(\s|$))|(&[^\s]+(\s|$))|[^\w\s\']|\d','',twt)# |(#[^\s]+(\s|$)) remove @ mentions, #, urls, special characters besides ' , and numbers
+		#print(twt)
+		twt = re.sub(r'(@[^\s]+(\s|$))|(#[^\s]+(\s|$))|(&[^\s]+(\s|$))|[^\w\s\']|\d','',twt)# |(#[^\s]+(\s|$)) remove @ mentions, #, urls, special characters besides ' , and numbers |(https?:\/\/[^\s]+(\s|$))
 		twt = re.sub(r'\s+',' ',twt)
 		twt = sym_spell.lookup_compound(twt,max_edit_distance=2)[0]._term
 		twt = twt.lower()
-		if not twt.isspace():
-			process.append(twt)
+		process.append(twt)
 		#print(twt)
 	nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
 	for doc in nlp.pipe(process, batch_size=20):
-		#print(doc)
 		preproc_pipe.append(lemmatize_pipe(doc))
+		# processed_doc = lemmatize_pipe(doc)
+		# if processed_doc:
+		# 	preproc_pipe.append(processed_doc)
 	return preproc_pipe
 
 def batch_lemmatizer(texts,chunksize=100):
-	executor = Parallel(n_jobs=4, backend='multiprocessing', prefer="processes")
+	executor = Parallel(n_jobs=6, backend='multiprocessing', prefer="processes")
 	do = delayed(chunk_processor)
 	tasks = (do(chunk) for chunk in chunker(texts, len(texts), chunksize=chunksize))
 	result = executor(tasks)
 	return flatten(result)
 
+
+if __name__ == "__main__":
+	client = pymongo.MongoClient('mongodb+srv://CISProjectUser:U1WsTu2X6fix49PA@cluster0.ttjkp.mongodb.net/test?authSource=admin&replicaSet=atlas-vvszkk-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true')
+	db = client['tweet_DB']
+	df = pd.DataFrame(list(db['labeled_Training_Data'].find({},{'_id':0,'text':1,'label':1})))
+	df['label'].replace(to_replace = 'pos',value = 4,inplace = True)
+	df['label'].replace(to_replace = 'neut',value = 2,inplace = True)
+	df['label'].replace(to_replace = 'neg',value = 0,inplace = True)
+	df['lema_text'] = (pd.Series(batch_lemmatizer(df['text'],50)))
+	df = df[df['lema_text'] != '']
+
+	db.processed_Training_Data_Three.insert_many(df.to_dict('records'))
+	# test2 = '@hiyorihere Chick-fil-A https://t.co/HBlpCzX4sq'
+	# print(str(batch_lemmatizer([test2])))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#     print(batch_lemmatizer([test_sentence]))
 #print(normalize_tweet(test_sentence))
 # nlp = spacy.load('en_core_web_sm', disable=['parser', 'ner'])
 # doc = nlp.pipe([test_sentence],batch_size=1)
@@ -161,22 +190,3 @@ def batch_lemmatizer(texts,chunksize=100):
 	# twt = re.sub(r'#[^\s]+(\s|$)','',twt) #remove #tags
 	# #print('removed tags: '+twt)
 	# twt = re.sub(r'https?://[^\s]+(\s|$)','',twt) # remove links
-
-
-#combine
-	#twt = re.sub(r'(@[^\s]+(\s|$))|(#[^\s]+(\s|$))|(https?://[^\s]+(\s|$))','',twt)# remove @ mentions
-
-
-#stop word removal done in scikit 
-	#	#clean_mess = [word for word in rm_tokens if not word.isspace() and word not in stopwords.words('english')]
-
-
-#symspell to be added if speedup can be done 
-	#a = ' '.join(l)
-	#r = sym_spell.lookup_compound(a, max_edit_distance = 2)
-	#r[0]._term
-
-
-#this was in something i read not sure why tho so i removed it 
-	#twt_list = [ele for ele in twtblob if ele != 'user']
-	#print('removed user: '+twt)
